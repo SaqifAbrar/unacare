102c59dd43712a9270db2b1feffc844d2896d4c2
Saqif Abrar <abrars@uoguelph.ca>
2022-07-16 17:19:56 -0500

    configured backend server

---
+++ app_node/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const apiRoutes = require("./src/routes/routes");
+
+//require("dotenv").config();
+//app.use(express.json());
+
+// location routes
+app.use("/api", apiRoutes);
+
+const port = process.env.PORT || 6000;
+app.listen(port, () =>
+	console.log(`UnaCare server is listening on port ${port}`),
+);
